db5eca9206bbde7b27421dfef72bf918812e4eee
DamBaoNhu <257140210090@hpu2.edu.vn>
2026-09-04 14:50:55 +0700

    202.cpp

---
+++ 202.cpp
@@ -0,0 +1,13 @@
+//Chuong trinh 2.2
+#include <iostream.h>
+int main()
+{
+	int X, Y;
+	cout<< "Nhap vao mot so X:";
+	cin>>X;
+	cout<< "Nhap vao mot so Y:";
+	cin>>Y;
+	cout<<"Tong cua chung:"<<X+Y<<"\n";//"\n" = endl ->cout<<endl;
+	cout<<"Hieu cua chung:"<<X-Y<<"\n";
+	return 0;
+}
